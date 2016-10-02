51f42f1186046b0e1d5697f8b0fd6873dfc036ee
talmaw19 <talmaw19@my.whitworth.edu>
2016-10-02 16:12:01 -0700

    Checks if it is in the range, added Catch ( range_errors &e)
    it now checks if it is inside the range and if not it says that it's not
    in the range. it also shows if symbols are not in the range so it askes
    to try agian until they are in range

--- Exception Handling and Unit-Testing/Exception Handling and Unit-Testing/Main.cpp
+++ Exception Handling and Unit-Testing/Exception Handling and Unit-Testing/Main.cpp
@@ -5,28 +5,34 @@ using namespace std;
 
 int main()
 {
-	
-	do {
-		try
-		{
-			int low, high;
+
+	int low, high;
 	cout << "Enter the range of values to read.\n";
 	cout << "high: ";
 	cin >> high;
 	cout << "low: ";
 	cin >> low;
-			int number = read_int("Please enter a number within the range: ", low, high);
-			cout << "You entered " << number << endl;
 
-			throw invalid_argument("out of range");
-		}
-		catch (invalid_argument &e) {
 
-			cerr << "Exception: You supplied an invalid argument for read_int! Try Again!   \n";
-		}
-	} while (true);
-	//	// try again
-	//	
-	//}
+	try
+	{
+		int number = read_int("Please enter a number within the range: ", low, high);
+		cout << "You entered " << number << endl;
+
+
+	}
+	catch (invalid_argument &e) {
+
+		cerr << "Exception: You supplied an invalid argument for read_int! Try Again! \n";
+	}
+	/*	catch (range_error &e)
+		{
+			cerr << " "
+		}*/
+	catch (ios_base::failure &ex) // 
+	{
+		cout << " you have entered out of the range of the number, please enter an integer, no symbols " << endl; 
+		cin.clear();
+		cin.ignore(numeric_limits<int>::max());
+	}
 }
-
--- Exception Handling and Unit-Testing/Exception Handling and Unit-Testing/readint.cpp
+++ Exception Handling and Unit-Testing/Exception Handling and Unit-Testing/readint.cpp
@@ -11,11 +11,25 @@ int read_int(const string &prompt, int low, int high)
 {
 	cin.exceptions(ios_base::failbit);
 	int num = 0;
+	if (low >= high) // it will check if low is greater than high 
+	{
+		throw (invalid_argument("low is greater than high.")); 
+	} 
+
 	while (true) { // Loop until valid input
 		try {
 			cout << prompt;
 			cin >> num;
+			// checks if the number inputed is in between low and high if not  
+			if (num <= low || num >= high)
+			{
+				throw (range_error("the number you have entered is outside the range ")); 
+			}
 			return num;
+		}
+		catch (const range_error &e)
+		{
+			cout << "the you entered is out of range please try again!" << endl;
 		}
 		catch (ios_base::failure& ex)
 		{
